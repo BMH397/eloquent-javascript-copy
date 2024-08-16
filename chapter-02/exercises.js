7a579a7162cf06f248a488a0cd35472d6fe69717
BMH397 <thereal397@gmail.com>
2024-08-16 11:55:57 -0500

    Could of sworn I did this already?

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -26,8 +26,10 @@ LOGS =>
 
 */
 
-function triangles() {
-  
+function triangles(n) {
+  for (let i = 1; i <= n; i++) {
+      console.log('#'.repeat(i));
+  }
 }
 
 
@@ -48,7 +50,40 @@ on the number:
 */
 
 function fizzBuzz(start, end) {
-  
+  //Initialize an array to store the results
+  const results = [];  
+
+  //Now loop through each number from 'start' to 'end'
+  for (let i = start; i <= end; i++) {
+      
+      //And if the current number i is divisible by both 3 and 5
+      if (i % 3 === 0 && i % 5 === 0) {
+      
+        //Push fizzbuzz to the results array    
+        results.push('fizzbuzz');  
+      } 
+      //And if the current number i is divisible by 3 only
+      else if (i % 3 === 0) {
+      
+        //Push fizz to the results array    
+        results.push('fizz');  
+      } 
+      //And if the current number i is divisible by 5 only
+      else if (i % 5 === 0) {
+      
+        //Push buzz to the results array    
+        results.push('buzz');  
+      } 
+      //If the current number i is not divisible by 3 or 5
+      else {
+       
+        // Push the number i to the results array  
+        results.push(i);  
+      }
+  }
+
+  // Log each result individually to match the expected output format
+  results.forEach(result => console.log(result));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -80,10 +115,40 @@ LOGS =>
  #
 
 */
+//Define a function named drawChessboard that takes one parameter size (This was so hard btw lol)
+function drawChessboard(size) {
+ 
+  //Make an empty string board that will hold the entire chessboard
+ let board = '';
+  //Loop from y = 0 to y < size incrementing y by 1 each time
+ for (let y = 0; y < size; y++) {
+   
+   //Make an empty string row for the current row
+   let row = '';
+    
+   //Now loop from x = 0 to x < size incrementing x by 1 each time
+   for (let x = 0; x < size; x++) {
+        
+     //Now check if the sum of x and y is even using the stricly equal operator
+     if ((x + y) % 2 === 0) {
+            
+       //And if true add a space ' ' to row
+       row += ' ';
+     
+       //If false
+     } else {
+            
+       // Add a # to row
+       row += '#';
+         }
+     }
 
-function drawChessboard(x) {
 
-
+     //Now add the row string to board followed by a newline character \n'
+     board += row + '\n';
+ }
+     //Now print board string to the console
+ console.log(board);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
